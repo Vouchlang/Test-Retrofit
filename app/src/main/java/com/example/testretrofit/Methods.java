8ec764cd0cb23560bdc577ecdeec3827aecd15be
Vouchlang <vouchlanggm@gmail.com>
2022-06-18 15:23:13 +0700

    Testing Retrofit

---
+++ app/src/main/java/com/example/testretrofit/Methods.java
@@ -0,0 +1,11 @@
+package com.example.testretrofit;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+
+public interface Methods {
+
+    @GET("api/users?page=2")
+    Call<Model> getAllData();
+
+}
